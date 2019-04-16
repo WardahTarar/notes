3110ee4ba91cf024883df12768412d1906e02a81
Wardah <wlt_93@hotmail.com>
2019-04-16 18:23:59 +0100

    Added app.js file. Created a function in note-controller that takes NoteListView data and displays notes on the web server

--- note-controller.js
+++ note-controller.js
@@ -1,10 +1,17 @@
 (function(exports) {
-  
-  var appContainer = document.getElementById('app')
 
-  var htmlString = appContainer.innerHTML
-  appContainer.innerHTML = "howdy"
-  console.log(appContainer.innerHTML)
+  function NoteController(notelist) {
+    this._notelist = notelist
+    // this._notelist.create("hello JS")
+    // this._notelist.create("i miss ruby")
+    this._notelistView = new NoteListView(this._notelist)
+  }
 
+  NoteController.prototype.displayNotes = function(){
+    var app = document.getElementById('app')
+    app.innerHTML = this._notelistView.getHTML()
+  }
+
+  exports.NoteController = NoteController
 
 })(this)

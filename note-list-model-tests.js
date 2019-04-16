e20bf70126c4b77a203f5d32f6d13b36aec1e873
Wardah <wlt_93@hotmail.com>
2019-04-16 15:05:58 +0100

    Removed commented out code

--- note-list-model-tests.js
+++ note-list-model-tests.js
@@ -1,20 +1,3 @@
-// (function(exports) {
-  
-//   function returnsAllNotes() {
-//     var notelist = new NoteList()
-//     // var note = new Note()
-
-//     if (notelist.view().length !== 1) {
-//       throw new Error("It doesn't have note")
-//     }
-
-//     if (notelist.create("First note")) {
-//       (notelist.view().includes("First note"))
-//     }
-//   }
-//   returnsAllNotes()
-// })(this)
-
 (function(exports){
   function storesNotesinList(){
     var notelist = new NoteList
